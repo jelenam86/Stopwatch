79c4a8d14095458a67247b9ee668eee713515af6
jelenam86 <jelena.mat86@gmail.com>
2019-07-20 20:23:04 +0200

    Added title to the stage

--- src/main/java/hellofx/SceneBuilderFX.java
+++ src/main/java/hellofx/SceneBuilderFX.java
@@ -6,9 +6,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -19,6 +19,7 @@ public class SceneBuilderFX extends Application {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("layout.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Stopwatch");
         stage.show();
     }
 
